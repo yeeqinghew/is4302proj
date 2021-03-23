41a30d75c9484bb9bf77f15f124b44538b877408
keith70000 <keith70000@hotmail.com>
2021-03-24 07:14:57 +0800

    Update MedicalRecords.sol

--- contracts/MedicalRecords.sol
+++ contracts/MedicalRecords.sol
@@ -4,20 +4,57 @@ contract MedicalRecords{
 
     // medical record structure
 
+    struct medicalRecord {
+        address patient;
+        bytes32 details;
+        address doctorInCharge;
+        mapping(uint256 => bool) viewerAccess;
+        uint256 cost;
+        
+    }
+
+    uint256 public numMedicalRecords = 0;
+    mapping(uint256 => medicalRecord) public medicalRecords;
+
     // medical record events
+    event createdMedicalRecord(uint256 medicalRecordId);
+
 
     // medical record modifiers
 
+
     // medical record functions
 
-    // function to create medical record
-    function createRecord(address patient, int cost) public {}
+    // function to create medical record (check that he is a dr and that patient exists)
+    function createRecord(address patient, bytes32 details, uint256 cost) public returns(uint256) {
+        
+        address[] memory viewerAccess;
+        medicalRecord memory newMedicalRecord = medicalRecord(
+            patient,
+            details, 
+            msg.sender,
+            viewerAccess,
+            cost
+        );
 
-    // function to view medical record
-    function viewRecord(int recordId) public {}
+        uint256 newMedicalRecordId = numMedicalRecords++;
+        medicalRecords[newMedicalRecordId] = newMedicalRecord;
+        emit createdMedicalRecord(newMedicalRecordId);
+        return newMedicalRecordId; 
+    }
 
-    // function to grant user access for this record
-    function grantAccess(address user) public {}
+    // function to view medical record (if inside viewaccess)
+    function viewRecord(uint256 medicalRecordId) public view returns(address, bytes32, address, uint256) {
+        require(medicalRecords[medicalRecordId].viewerAccess[msg.sender].exists == true, "Not authorised to view medical record.");
+
+        return (medicalRecords[medicalRecordId].patient, medicalRecords[medicalRecordId].details, medicalRecords[medicalRecordId].doctorInCharge, medicalRecords[medicalRecordId].cost);
+    }
+
+    // function to grant user access for this record 
+    function grantAccess(address user, uint256 medicalRecordId) public {
+        require(medicalRecords[medicalRecordId].viewerAccess[msg.sender].exists == false, "Already authorised to view medical record.");
+        medicalRecords[medicalRecordId].viewerAccess.push(user);
+    }
 
     // function to remove user access for this record
     function ungrantAccess(address user) public {}
