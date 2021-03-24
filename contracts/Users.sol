74c972975e893cd90b9fcce7c30d019a31adef37
keith70000 <keith70000@hotmail.com>
2021-03-24 09:49:23 +0800

    added register unregister functions

--- contracts/Users.sol
+++ contracts/Users.sol
@@ -19,6 +19,11 @@ contract Users{
     event registeredHealthcareProvider(address healthcareProvider);
     event registeredNurse(address nurse);
     event registeredFinancialInstitute(address financialInstitute);
+    event unregisteredAnalyst(address analyst);
+    event unregisteredDoctor(address doctor);
+    event unregisteredHealthcareProvider(address healthcareProvider);
+    event unregisteredNurse(address nurse);
+    event unregisteredFinancialInstitute(address financialInstitute);
 
     // user modifiers
     modifier adminOnly(address user) {
@@ -36,37 +41,75 @@ contract Users{
     // user functions
 
     // function to register patient (only by dr/nurse)
-    function registerPatient(address patient) public adminOnly(msg.sender) {}
+    function registerPatient(address patient) public adminOnly(msg.sender) {
+        patients[patient] = true;
+        emit registeredPatient(patient);
+    }
 
     // function to register analyst (only by hcp)
-    function registerAnalyst(address analyst) public healthcareProviderOnly(msg.sender) {}
+    function registerAnalyst(address analyst) public healthcareProviderOnly(msg.sender) {
+        analysts[analyst] = true;
+        emit registeredAnalyst(analyst);
+    }
 
     // function to register doctor (only by hcp)
-    function registerDoctor(address doctor) public healthcareProviderOnly(msg.sender) {}
+    function registerDoctor(address doctor) public healthcareProviderOnly(msg.sender) {
+        doctors[doctor] = true;
+        emit registeredDoctor(doctor);
+    }
 
     // function to register healthcare provider (only by admin)
-    function registerHealthcareProvider(address provider) public adminOnly(msg.sender) {}
+    function registerHealthcareProvider(address provider) public adminOnly(msg.sender) {
+        healthcareProviders[provider] = true;
+        emit registeredHealthcareProvider(provider);
+    }
 
     // function to register nurse (only by hcp)
-    function registerNurse(address nurse) public healthcareProviderOnly(msg.sender) {}
+    function registerNurse(address nurse) public healthcareProviderOnly(msg.sender) {
+        nurses[nurse] = true;
+        emit registeredNurse(nurse);
+    }
 
     // function to register financial institute (only by admin)
-    function registerFinancialInstitute(address institute) public adminOnly(msg.sender) {}
+    function registerFinancialInstitute(address institute) public adminOnly(msg.sender) {
+        financialInstitutes[institute] = true;
+        emit registeredFinancialInstitute(institute);
+    }
 
     // function to unregister analyst
-    function unregisterAnalyst(address analyst) public healthcareProviderOnly(msg.sender) {}
+    function unregisterAnalyst(address analyst) public healthcareProviderOnly(msg.sender) {
+        require(analysts[analyst] == true, "No such analyst.");
+        delete analysts[analyst];
+        emit unregisteredAnalyst(analyst);
+    }
 
     // function to unregister doctor
-    function unregisterDoctor(address doctor) public healthcareProviderOnly(msg.sender) {}
+    function unregisterDoctor(address doctor) public healthcareProviderOnly(msg.sender) {
+        require(doctors[doctor] == true, "No such doctor.");
+        delete doctors[doctor];
+        emit unregisteredDoctor(doctor);
+    }
 
     // function to unregister healthcare provider
-    function unregisterHealthcareProvider(address provider) public adminOnly(msg.sender) {}
+    function unregisterHealthcareProvider(address provider) public adminOnly(msg.sender) {
+        require(healthcareProviders[provider] == true, "No such healthcare provider.");
+        delete healthcareProviders[provider];
+        emit unregisteredHealthcareProvider(provider);
+    }
 
     // function to unregister nurse
-    function unregisterNurse(address nurse) public healthcareProviderOnly(msg.sender) {}
+    function unregisterNurse(address nurse) public healthcareProviderOnly(msg.sender) {
+        require(nurses[nurse] == true, "No such nurse.");
+        delete nurses[nurse];
+        emit unregisteredNurse(nurse);
+    }
 
     // function to unregister financial institute
-    function unregisterFinancialInstitute(address institute) public adminOnly(msg.sender) {}
+    function unregisterFinancialInstitute(address institute) public adminOnly(msg.sender) {
+        require(financialInstitutes[institute] == true, "No such financial institute.");
+        delete financialInstitutes[institute];
+        emit unregisteredFinancialInstitute(institute);
+    }
 
     // function to check if address is patient
     function isPatient(address user) public view returns(bool) {
