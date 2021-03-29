91bc97cbd90073ed5e52e947ba4212a2f6796184
ZiHua <e0335699@u.nus.edu>
2021-03-29 21:31:43 +0800

    edit index.js

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -25,7 +25,9 @@ db.sequelize = sequelize;
 
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
-// db.patient = require("../models/patient.model.js")(sequelize, Sequelize);
+db.patient = require("../models/patient.model.js")(sequelize, Sequelize);
+db.doctor = require("../models/doctor.model.js")(sequelize, Sequelize);
+db.admin = require("../models/admin.model.js")(sequelize, Sequelize);
 // this will be the DATABASE Schema
 
 // role can be owned by multiple user
@@ -35,8 +37,38 @@ db.role.hasMany(db.user, {
 // user can have only ONE role
 db.user.belongsTo(db.role, {
     as: "role",
-    foreignKey: "roleId"
+    foreignKey: "roleId" //add "roleId" attribute to User
 });
 
-db.ROLES = ["patient", "admin", "doctor"];
+// user can only be owned by ONE patient
+db.user.hasOne(db.patient, {
+    as: "patient"
+});
+// patient belongs to ONE user
+db.patient.belongsTo(db.user, {
+    as: "user",
+    foreignKey: "userId" //add "userId" attribute to Patient
+});
+
+// user can only be owned by ONE doctor
+db.user.hasOne(db.doctor, {
+    as: "doctor"
+});
+// doctor belongs to ONE user
+db.doctor.belongsTo(db.user, {
+    as: "user",
+    foreignKey: "userId" //add "userId" attribute to Doctor
+});
+
+// user can only be owned by ONE admin
+db.user.hasOne(db.admin, {
+    as: "admin"
+});
+// admin belongs to ONE user
+db.admin.belongsTo(db.user, {
+    as: "user",
+    foreignKey: "userId" //add "userId" attribute to Admin
+});
+
+db.ROLES = ["patient", "doctor", "admin"];
 module.exports = db;
