a24cbd624889aa21ce465eb1a98c6f950a96c572
yeeqing <e0318909@u.nus.edu>
2021-04-10 21:09:50 +0800

    git pull for viewRecordAdmin

--- client/src/components/Doctor/RandomRecords.js
+++ client/src/components/Doctor/RandomRecords.js
@@ -12,7 +12,7 @@ class RandomRecords extends Component {
   constructor(props) {
     super(props);
     this.getRandomInt = this.getRandomInt.bind(this);
-    this.retrievePatientData = this.retrievePatientData.bind(this);
+    this.retrieveUserData = this.retrieveUserData.bind(this);
     this.retrieveRandomRecords = this.retrieveRandomRecords.bind(this);
     this.viewRecord = this.viewRecord.bind(this);
 
@@ -65,122 +65,151 @@ class RandomRecords extends Component {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  retrievePatientData = async(patientId) => {
+  retrieveUserData = async (patientId, doctorId) => {
     var patient;
+    var doctor;
 
     await UserService.getPatientById(patientId).then(
-        response => {
-            patient = response.data;
-        },
-        error => {
-            console.log(
-                (error.response &&
-                    error.response.data &&
-                    error.response.data.message) ||
-                error.message ||
-                error.toString())
-        }
+      (response) => {
+        patient = response.data;
+      },
+      (error) => {
+        console.log(
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+            error.message ||
+            error.toString()
+        );
+      }
     );
-    return { patientData: patient};
-}
+    await UserService.getDoctorById(doctorId).then(
+      (response) => {
+        doctor = response.data;
+      },
+      (error) => {
+        console.log(
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+            error.message ||
+            error.toString()
+        );
+      }
+    );
+    return { patientData: patient, doctorData: doctor };
+  };
 
-retrieveRandomRecords = async () => {
-    const { web3, accounts, medicalRecordContract} = this.state;
+  retrieveRandomRecords = async () => {
+    const { web3, accounts, medicalRecordContract } = this.state;
 
-    const numRecords = await medicalRecordContract.methods.getNumMedicalRecords().call();
+    const { user: currentDoctor } = this.props;
+    console.log("user", currentDoctor);
+
+    const numRecords = await medicalRecordContract.methods
+      .getNumMedicalRecords()
+      .call();
     console.log("numRecords:", numRecords);
 
     var min = 0;
     var max = numRecords;
     var medicalRecords = [];
     while (min < max) {
-        min = this.getRandomInt(min, max) + 1;
-        console.log("min", min);
+      min = this.getRandomInt(min, max) + 1;
+      console.log("min", min);
 
-        if (min < max) {
-            const response = await medicalRecordContract.methods.viewRecord(min).call({from: accounts[0]});
-            if (response[3] === '0') { // unflagged
-                const { patientData } = await this.retrievePatientData(response[0]);
-                console.log("patientData", patientData);
-                
-                var record = {
-                    recordId: min,
-                    patient: response[0], 
-                    details: web3.utils.hexToAscii(response[1]),
-                    patientVerified: response[2], 
-                    doctorVerified: response[3],
-                    patientData: patientData
-                };
-                medicalRecords.push(record);
-                // console.log("record:", record);
-            }
+      if (min < max) {
+        const response = await medicalRecordContract.methods
+          .viewRecord(min)
+          .call({ from: accounts[0] });
+        if (response[4] == 0 && response[2] != currentDoctor.doctorId) {
+          // unflagged, not owned by current doctor
+          const { patientData, doctorData } = await this.retrieveUserData(
+            response[0],
+            response[2]
+          );
+          console.log("patientData", patientData);
+          console.log("doctorData", doctorData);
+
+          var record = {
+            recordId: min,
+            patient: response[0],
+            details: web3.utils.hexToAscii(response[1]),
+            doctorInCharge: response[2],
+            patientVerified: response[3],
+            doctorVerified: response[4],
+            patientData: patientData,
+            doctorData: doctorData,
+          };
+          medicalRecords.push(record);
+          // console.log("record:", record);
         }
-        
+      }
     }
     console.log("***** Medical Records:", medicalRecords);
     this.setState({
-        medicalRecords: medicalRecords,
-        loading: true
+      medicalRecords: medicalRecords,
+      loading: true,
     });
-}
+  };
 
-viewRecord(recordId) {
-    localStorage.setItem('recordId', recordId);
+  viewRecord(recordId) {
+    localStorage.setItem("recordId", recordId);
 
     let path = "/verifyRecord";
     this.props.history.push(path);
-}
+  }
 
-render() {
+  render() {
     const { medicalRecords, loading } = this.state;
     return (
-        <Fragment>
-            <header className="jumbotron">
-            <h1> Medical Records </h1>
-            </header>
-            {loading && medicalRecords.length !== 0 && (
-                <Table hover responsive striped>
-                {/* <table className="table table-hover table-responsive" style={{width: '100%', margin: '0px'}}> */}
-                    <thead>
-                        <tr>
-                        <th>Record ID</th>
-                        <th>Patient</th>
-                        <th>Details</th>
-                        <th>Patient Verified</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {medicalRecords.map((record) => (
-                            <tr
-                                key = {record.recordId}
-                                onClick = {(e) => {
-                                    console.log("recordId: ", record.recordId);
-                                    this.viewRecord(record.recordId);
-                                }}
-                            >
-                            <th>{record.recordId}</th>
-                            <th>{record.patientData.first_name + " " + record.patientData.last_name}</th>
-                            <th>{(record.details.length > 15) ? record.details.substr(0, 14) + '...' : record.details}</th>
-                            <th>{(() => {
-                                    switch(record.patientVerified) {
-                                        case '0': return "Not Flagged";
-                                        case '1': return "Verified";
-                                        case '2': return "Flagged"
-                                    }
-                                })()}  
-                            </th>
-                            </tr>
-                        ))}
-                    </tbody>
-                    {/* </table> */}
-                </Table>
-            )}
-            {loading && medicalRecords.length === 0 && (
-                <h5>No records to verify</h5>
-            )}
-        </Fragment>
+      <Fragment>
+        <header className="jumbotron">
+          <h1> Medical Records </h1>
+        </header>
+        {loading && medicalRecords.length != 0 && (
+          <Table hover responsive striped>
+            {/* <table className="table table-hover table-responsive" style={{width: '100%', margin: '0px'}}> */}
+            <thead>
+              <tr>
+                <th>Record ID</th>
+                <th>Patient</th>
+                <th>Doctor-in-Charge</th>
+                <th>Specialty</th>
+                <th>Healthcare Institution</th>
+              </tr>
+            </thead>
+            <tbody>
+              {medicalRecords.map((record) => (
+                <tr
+                  key={record.recordId}
+                  onClick={(e) => {
+                    console.log("recordId: ", record.recordId);
+                    this.viewRecord(record.recordId);
+                  }}
+                >
+                  <th>{record.recordId}</th>
+                  <th>
+                    {record.patientData.first_name +
+                      " " +
+                      record.patientData.last_name}
+                  </th>
+                  <th>
+                    {record.doctorData.first_name +
+                      " " +
+                      record.doctorData.last_name}
+                  </th>
+                  <th>{record.doctorData.specialty}</th>
+                  <th>{record.doctorData.healthcare_institution}</th>
+                </tr>
+              ))}
+            </tbody>
+            {/* </table> */}
+          </Table>
+        )}
+        {loading && medicalRecords.length == 0 && <h5>No records to verify</h5>}
+      </Fragment>
     );
-    
   }
 }
 
